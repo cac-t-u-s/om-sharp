8d9d1e643762a199880a1e3e4ee6d23c0fe148ce
dimitribouche <dimitri.bouche@ircam.fr>
2016-11-02 11:45:30 +0100

    Fix ruler when loading old maquette ?

--- OPENMUSIC/src/kernel/graphics/om-rulers.lisp
+++ OPENMUSIC/src/kernel/graphics/om-rulers.lisp
@@ -19,6 +19,12 @@
 
 (defmethod set-shift-and-factor ((self t)) nil)
 
+(defmethod x1 ((self x-graduated-view))
+  (or (slot-value self 'x1) 0))
+
+(defmethod x2 ((self x-graduated-view))
+  (or (slot-value self 'x2) 0))
+
 (defmethod set-shift-and-factor ((self x-graduated-view))
   (let ((range (- (x2 self) (x1 self))))
     (unless (zerop range)
