22717b3e4d6565cf4afced982005085576ac608e
Jean Bresson <bresson@ircam.fr>
2016-11-27 18:13:15 +0100

    2D array col/lines doc

--- OPENMUSIC/src/packages/basic/containers/2d-array.lisp
+++ OPENMUSIC/src/packages/basic/containers/2d-array.lisp
@@ -2,10 +2,10 @@
 (in-package :om)
 
 (defclass* OMArray () 
-   ((num-elts :initform 1 :initarg :num-elts  :accessor num-elts :documentation "number of elements")
-    (num-fields :initform 1 :initarg :num-fields  :accessor num-fields :documentation "number of fields")
+   ((num-elts :initform 1 :initarg :num-elts  :accessor num-elts :documentation "number of elements (a.k.a lines)")
+    (num-fields :initform 1 :initarg :num-fields  :accessor num-fields :documentation "number of fields (a.k.a columns)")
     (field-names :initform nil :initarg :field-names :accessor field-names :documentation "name of fields")
-    (data :initform nil :initarg :data :accessor data :documentation "data matrix (2D / list of lists)")))
+    (data :initform nil :initarg :data :accessor data :documentation "data matrix / list of lists : (col1 col2 ...)")))
 
 (defmethod get-col ((self OMArray) (col integer))
   (nth col (data self)))
