3f18f51f26532a5263c383382299f14568fc3e95
Jean Bresson <bresson@ircam.fr>
2016-12-21 12:35:28 +0100

    move improtek to libraries

--- OPENMUSIC/src/packages/improtek/improtek/sources/1-Load-Init/WorkDirectory.lisp
+++
@@ -1,4 +0,0 @@
-(in-package :om)
-(setf path_work_directory #P"/Users/bouche/Documents/OpenMusic/OM7/OPENMUSIC/code/api/om-api-LW/")
-
-;To modify the path of the work directory, close this file and call (change_path_directory)
