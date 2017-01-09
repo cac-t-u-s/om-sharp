82b65d561ba93ec77fef23e9590b75c112b0b6cb
dimitribouche <dimitri.bouche@ircam.fr>
2017-01-09 17:18:05 +0100

    New AudioLib

--- OPENMUSIC/src/packages/sound/audio-api/omjuceaudiolib/OMJuceAudioLib.lisp
+++ OPENMUSIC/src/packages/sound/audio-api/omjuceaudiolib/OMJuceAudioLib.lisp
@@ -14,9 +14,16 @@
 ;;;==============================================
 ;;  PLAYER
 ;;;==============================================
-(cffi:defcfun ("OpenAudioPlayer" OpenAudioPlayer) :pointer (inchannels :int) (outchannels :int) (samplerate :int))
+(cffi:defcfun ("OpenAudioPlayer" OpenAudioPlayer) :pointer)
 
-;(openaudioplayer 2 2 44100)
+(cffi:defcfun ("GetAvailableInputDevices" GetAvailableInputDevices) :pointer (player :pointer))
+(cffi:defcfun ("GetAvailableOutputDevices" GetAvailableOutputDevices) :pointer (player :pointer))
+(cffi:defcfun ("getInputDevicesCount" getInputDevicesCount) :int (player :pointer))
+(cffi:defcfun ("getOutputDevicesCount" getOutputDevicesCount) :int (player :pointer))
+(cffi:defcfun ("setAudioDevice" setAudioDevice) :void 
+  (player :pointer) (inputdevicename :pointer) (outputdevicename :pointer) (inchan :int) (outchan :int) (sr :int))
+
+;(cffi:foreign-string-to-lisp (fli:dereference (scandevices) :index 1 :type :pointer))
 
 (cffi:defcfun ("CloseAudioPlayer" CloseAudioPlayer) :void (player :pointer))
 
@@ -42,3 +49,9 @@
 (cffi:defcfun ("GetPosReader" GetPosReader) :long (reader :pointer))
 
 (cffi:defcfun ("LoopReader" LoopReader) :void (reader :pointer) (looper :boolean))
+
+(cffi:defcfun ("GetGainReader" GetGainReader) :float (reader :pointer))
+
+(cffi:defcfun ("SetGainReader" SetGainReader) :float (reader :pointer) (gain :float))
+
+
--- OPENMUSIC/src/packages/sound/sound/juce-player.lisp
+++ OPENMUSIC/src/packages/sound/sound/juce-player.lisp
@@ -13,11 +13,53 @@
 #+omjuceaudiolib(enable-player :omjuceaudiolib)
 
 (defvar *juce-player* nil)
-(defvar *juce-player-n-channels* 2)
+(defvar *juce-player-in-channels* 0)
+(defvar *juce-player-out-channels* 2)
+(defvar *juce-input-devices* nil)
+(defvar *juce-output-devices* nil)
 
-(defun open-juce-player (&optional (sr 44100))
-  (progn
-    (setf *juce-player* (juce::OpenAudioPlayer *juce-player-n-channels* *juce-player-n-channels* sr))))
+(setf *juce-input-devices* nil)
+
+(add-preference-module :audio "Audio")
+(add-preference :audio :input "Input device" *juce-input-devices* (car *juce-input-devices*))
+(add-preference :audio :input "Output device" *juce-output-devices* (car *juce-output-devices*))
+(add-preference :audio :apply "Apply" :action 'om-beep)
+
+(get-pref-value :audio :input)
+
+(defun open-juce-player ()
+  (setq *juce-player* (juce::OpenAudioPlayer)
+        *juce-input-devices* (loop for i from 0 to (1- (juce::getinputdevicescount *juce-player*))
+                                   collect
+                                   (cffi:foreign-string-to-lisp (fli:dereference (juce::getavailableinputdevices *juce-player*)
+                                                                                 :index i :type :pointer)))
+        *juce-output-devices* (loop for i from 0 to (1- (juce::getoutputdevicescount *juce-player*))
+                                    collect
+                                    (cffi:foreign-string-to-lisp (fli:dereference (juce::getavailableoutputdevices *juce-player*)
+                                                                                  :index i :type :pointer)))))
+
+(defun set-juce-device (input-device-index output-device-index sample-rate)
+  (cffi::with-foreign-pointer-as-string (str 255)
+    (juce::setaudiodevice *juce-player* 
+                          (cffi::lisp-string-to-foreign (nth input-device-index *juce-input-devices*)
+                                                        str (1+ (length (nth input-device-index *juce-input-devices*))))
+                          (cffi::lisp-string-to-foreign (nth output-device-index *juce-output-devices*)
+                                                        str (1+ (length (nth output-device-index *juce-output-devices*)))) 
+                          *juce-player-in-channels* 
+                          *juce-player-out-channels*
+                          sample-rate)))
+
+
+
+
+
+;(set-juce-device 0 0 44100) A APPELER
+
+
+
+;(listen *terminal-io*) (defun testget (a) (setq *testbp* (bp-pointer (buffer-player a))))
+
+;(juce::setgainreader *testbp* 1.0)
 
 (defun close-juce-player ()
   (juce::closeaudioplayer *juce-player*)
