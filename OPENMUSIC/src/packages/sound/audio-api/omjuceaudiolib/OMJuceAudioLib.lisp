75e28043f6dc41b53df32948d9c458a2ae77c651
Jean Bresson <bresson@ircam.fr>
2017-01-17 11:16:54 +0100

    juce audio player in progress..

--- OPENMUSIC/src/packages/sound/audio-api/omjuceaudiolib/OMJuceAudioLib.lisp
+++ OPENMUSIC/src/packages/sound/audio-api/omjuceaudiolib/OMJuceAudioLib.lisp
@@ -14,11 +14,14 @@
 ;;;==============================================
 ;;  PLAYER
 ;;;==============================================
-(cffi:defcfun ("OpenAudioPlayer" OpenAudioPlayer) :pointer)
-
+(cffi:defcfun ("openAudioManager" openAudioManager) :pointer)
+(cffi:defcfun ("closeAudioManager" closeAudioManager) :void (player :pointer))
 
 (cffi:defcfun ("getDevicesTypeCount" getDevicesTypeCount) :int (player :pointer))
 (cffi:defcfun ("getDeviceTypeName" getDeviceTypeName) :string (player :pointer) (type :int))
+(cffi:defcfun ("setDeviceType" setDeviceType) :void (player :pointer) (type :string))
+(cffi:defcfun ("getCurrentDeviceType" getCurrentDeviceType) :string (player :pointer))
+
 (cffi:defcfun ("getInputDevicesCountForType" getInputDevicesCountForType) :int (player :pointer) (type :int))
 (cffi:defcfun ("getOutputDevicesCountForType" getOutputDevicesCountForType) :int (player :pointer) (type :int))
 (cffi:defcfun ("getNthInputDeviceName" getNthInputDeviceName) :string (player :pointer) (type :int) (n :int))
@@ -26,12 +29,11 @@
 (cffi:defcfun ("getInputDevicesCount" getInputDevicesCount) :int (player :pointer))
 (cffi:defcfun ("getOutputDevicesCount" getOutputDevicesCount) :int (player :pointer))
 
-(cffi:defcfun ("GetAvailableInputDevices" GetAvailableInputDevices) :pointer (player :pointer))
-(cffi:defcfun ("GetAvailableOutputDevices" GetAvailableOutputDevices) :pointer (player :pointer))
-
+;(cffi:defcfun ("setAudioDevice" setAudioDevice) :void 
+;  (player :pointer) (outputdevicename :string) (inputdevicename :string)  (inchan :int) (outchan :int) (sr :int) (buffsize :int))
 (cffi:defcfun ("setAudioDevice" setAudioDevice) :void 
-  (player :pointer) (inputdevicename :pointer) (outputdevicename :pointer) (inchan :int) (outchan :int) (sr :int) (buffsize :int))
-;;; todo : use cffi :string type
+  (player :pointer) (output :int) (input :int)  (in-channels :int) (out-channels :int) (sr :int) (buffsize :int))
+
 (cffi:defcfun ("GetAvailableSampleRates" GetAvailableSampleRates) :pointer (player :pointer))
 (cffi:defcfun ("GetAvailableSampleRatesCount" GetAvailableSampleRatesCount) :int (player :pointer))
 (cffi:defcfun ("GetAvailableBufferSizes" GetAvailableBufferSizes) :pointer (player :pointer))
@@ -39,37 +41,56 @@
 (cffi:defcfun ("GetDefaultBufferSize" GetDefaultBufferSize) :int (player :pointer))
 (cffi:defcfun ("getInputChannelsCount" GetInputChannelsCount) :int (player :pointer))
 (cffi:defcfun ("getOutputChannelsCount" GetOutputChannelsCount) :int (player :pointer))
-(cffi:defcfun ("setActiveOutputChannels" setActiveOutputChannels) :int (player :pointer) (n :int) (mask :pointer))
+(cffi:defcfun ("setOutputChannelsMapping" setOutputChannelsMapping) :int (player :pointer) (n :int) (map :pointer))
+
+;;; SCAN UTILITIES (INDEPENDENT ON THE CURRENT SETUP)
+(defun get-audio-drivers (audiomanager)
+  (let ((n-types (juce::getDevicesTypeCount audiomanager)))
+    (loop for type from 0 to (1- n-types) collect
+          (juce::getDeviceTypeName audiomanager type))))
+
+(defun get-all-audio-output-devices (audiomanager)
+  (let ((n-types (juce::getDevicesTypeCount audiomanager)))
+    (loop for type from 0 to (1- n-types) append
+          (let ((type-name (juce::getDeviceTypeName audiomanager type)))
+            (loop for n from 0 to (1- (juce::getOutputDevicesCountForType audiomanager type)) 
+                    collect (juce::getNthOutputDeviceName audiomanager type n)
+                    )))))
+
+(defun get-all-audio-input-devices (audiomanager)
+  (let ((n-types (juce::getDevicesTypeCount audiomanager)))
+    (loop for type from 0 to (1- n-types) append
+          (let ((type-name (juce::getDeviceTypeName audiomanager type)))
+            (loop for n from 0 to (1- (juce::getInputDevicesCountForType audiomanager type)) 
+                    collect (juce::getNthInputDeviceName audiomanager type n)
+                    )))))
+
+(defun audio-driver-output-devices (audiomanager driver)
+  (let ((type-num (position driver (get-audio-drivers audiomanager) :test 'string-equal)))
+    (if type-num
+        (loop for n from 0 to (1- (juce::getOutputDevicesCountForType audiomanager type-num)) 
+              collect (juce::getNthOutputDeviceName audiomanager type-num n))
+      (error "Audio driver ~S not found." driver))))
+
+(defun audio-driver-input-devices (audiomanager driver)
+  (let ((type-num (position driver (get-audio-drivers audiomanager) :test 'string-equal)))
+    (if type-num
+        (loop for n from 0 to (1- (juce::getInputDevicesCountForType audiomanager type-num)) 
+              collect (juce::getNthInputDeviceName audiomanager type-num n))
+      (error "Audio driver ~S not found." driver))))
 
 (defun setoutputchannels (player activechannelslist)
   (let* ((l (length activechannelslist))
-         (mask (cffi:foreign-alloc :int :count l :initial-contents (mapcar '1- activechannelslist))))
+         (map (cffi:foreign-alloc :int :count l :initial-contents (mapcar '1- activechannelslist))))
     (unwind-protect 
-        (progn
-          ;(loop for from 0 to (1- l) do
-          ;      (setf (fli:dereference mask :index (1- ch) :type :int) 1))
-          (setActiveOutputChannels player l mask))
-      (cffi-sys:foreign-free mask))))
+        (setoutputchannelsmapping player l map)
+      (cffi-sys:foreign-free map))))
       
-
-;(cffi:foreign-string-to-lisp (fli:dereference (scandevices) :index 1 :type :pointer))
-
 (defun getinputchannelslist (player)
   (or (loop for i from 1 to (juce::GetInputChannelsCount player) collect i) '(0)))
 
 (defun getoutputchannelslist (player)
   (or (loop for i from 1 to (juce::GetOutputChannelsCount player) collect i) '(0)))
-
-(defun getinputdevicenames (player)
-  (loop for i from 0 to (1- (juce::getinputdevicescount player))
-        collect
-        (cffi:foreign-string-to-lisp (fli:dereference (juce::getavailableinputdevices player)
-                                                      :index i :type :pointer))))
-(defun getoutputdevicenames (player)
-  (loop for i from 0 to (1- (juce::getoutputdevicescount player))
-        collect
-        (cffi:foreign-string-to-lisp (fli:dereference (juce::getavailableoutputdevices player)
-                                                      :index i :type :pointer))))
 
 (defun getsamplerates  (player)
   (loop for i from 0 to (1- (juce::getavailablesampleratescount player))
@@ -83,18 +104,18 @@
 
 ;;; probleme abvec les caractères accentués !!
 (defun setdevices (player input-device-name inch output-device-name outch sample-rate buffer-size)
-  (cffi::with-foreign-pointer-as-string (str 255)
-    (juce::setaudiodevice player 
-                          (cffi::lisp-string-to-foreign input-device-name str (1+ (length input-device-name)))
-                          (cffi::lisp-string-to-foreign output-device-name str (1+ (length output-device-name)))
-                          inch 
-                          outch
-                          sample-rate
-                          buffer-size)))
+  (let* ((driver (getCurrentDeviceType player))
+         (in-n (or (position input-device-name 
+                             (audio-driver-output-devices player driver) 
+                             :test 'string-equal) 0))
+         (out-n (or (position output-device-name 
+                              (audio-driver-input-devices player driver) 
+                              :test 'string-equal) 0)))
+    (juce::setaudiodevice player in-n out-n inch outch sample-rate buffer-size)))
 
-
-(cffi:defcfun ("CloseAudioPlayer" CloseAudioPlayer) :void (player :pointer))
-
+;(convert-string input-device-name)
+;(cffi::lisp-string-to-foreign input-device-name str (1+ (length input-device-name)))
+;(setf str (getNthOutputDeviceName om::*juce-player* 0 3))
 
 ;;;==============================================
 ;;  BUFFER
@@ -102,23 +123,14 @@
 
 (cffi:defcfun ("MakeDataReader" MakeDataReader) :pointer (buffer :pointer) (channels :int) (size :int) (sr :int))
 (cffi:defcfun ("MakeFileReader" MakeFileReader) :pointer (file :string))
-
 (cffi:defcfun ("FreeReader" FreeReader) :void (reader :pointer))
-
 (cffi:defcfun ("StartReader" StartReader) :void (player :pointer) (reader :pointer))
-
 (cffi:defcfun ("PauseReader" PauseReader) :void (player :pointer) (reader :pointer))
-
 (cffi:defcfun ("StopReader" StopReader) :void (player :pointer) (reader :pointer))
-
 (cffi:defcfun ("SetPosReader" SetPosReader) :void (reader :pointer) (pos :long))
-
 (cffi:defcfun ("GetPosReader" GetPosReader) :long (reader :pointer))
-
 (cffi:defcfun ("LoopReader" LoopReader) :void (reader :pointer) (looper :boolean))
-
 (cffi:defcfun ("GetGainReader" GetGainReader) :float (reader :pointer))
-
 (cffi:defcfun ("SetGainReader" SetGainReader) :void (reader :pointer) (gain :float))
 
 
--- OPENMUSIC/src/packages/sound/sound/juce-player.lisp
+++ OPENMUSIC/src/packages/sound/sound/juce-player.lisp
@@ -13,6 +13,8 @@
 #+omjuceaudiolib(enable-player :omjuceaudiolib)
 
 (defvar *juce-player* nil)
+(defvar *audio-driver* "CoreAudio")
+
 ;(defvar *juce-player-in-channels* 0)
 ;(defvar *juce-player-out-channels* 2)
 ;(defvar *juce-sample-rate* 44100)
@@ -24,7 +26,7 @@
 (add-preference-module :audio "Audio")
 (add-preference-section :audio "Devices")
 ;(add-preference :audio :input "Input device" *juce-input-devices* (car *juce-input-devices*))
-(add-preference :audio :output "Output device" nil nil nil 'apply-audio-device-selected) ;; will be set at player startup
+(add-preference :audio :output "Output device" nil nil nil 'set-audio-device) ;; will be set at player startup
 (add-preference-section :audio "Configuration")
 ;(add-preference :audio :in-channels "Input Channels" '(0) 0)
 (add-preference :audio :out-channels "Output Channels" '(2) 2 nil 'apply-audio-device-config)
@@ -33,63 +35,33 @@
 
 ; (add-preference :audio :apply "Apply" :action 'apply-audio-prefs)
 
-;(juce::getinputdevicescount *juce-player*)
-;(juce::getoutputdevicescount *juce-player*)
-;(cffi:foreign-string-to-lisp (fli:dereference (juce::getavailableoutputdevices *juce-player*) :index 2 :type :pointer))
-
-;; todo : only one type of device can be selected at a time.
-
-(defun audio-output-devices ()
-  (when *juce-player*
-    (let ((n-types (juce::getDevicesTypeCount *juce-player*)))
-      (loop for type from 0 to (1- n-types) append
-            (let ((type-name (juce::getDeviceTypeName *juce-player* type)))
-              (loop for n from 0 to (1- (juce::getOutputDevicesCountForType *juce-player* type)) 
-                    collect 
-                    ;(list type-name (getNthOutputDeviceName *juce-player* type n))
-                    (juce::getNthOutputDeviceName *juce-player* type n)
-                    ))))))
-
-(defun audio-input-devices ()
-  (when *juce-player*
-    (let ((n-types (juce::getDevicesTypeCount *juce-player*)))
-      (loop for type from 0 to (1- n-types) append
-            (let ((type-name (juce::getDeviceTypeName *juce-player* type)))
-              (loop for n from 0 to (1- (juce::getInputDevicesCountForType *juce-player* type)) 
-                    collect (juce::getNthInputDeviceName *juce-player* type n))
-              )))))
-
 (defun default-audio-input-device ()
-  (or (car (audio-input-devices)) ""))
+  (or (and *juce-player* (car (audio-driver-input-devices *juce-player* *audio-driver*))) ""))
 
 (defun default-audio-output-device ()
-  (or (car (audio-output-devices)) ""))
+  (or (and *juce-player* (car (audio-driver-output-devices *juce-player* *audio-driver*))) ""))
 
-; (juce::setoutputchannels *juce-player* '(5 6 7 8 1 2 3 4 9 10 11 12 13 14 15 16))
-; (juce::setoutputchannels *juce-player* '(5 6 7 8 5 6)) ; 
-; (juce::setoutputchannels *juce-player* '(1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16))
-; (juce::setoutputchannels *juce-player* '(0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0))
-; (juce::setoutputchannels *juce-player* '(1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1))
+; (juce::get-audio-drivers *juce-player*)
 
-; (juce::setoutputchannels *juce-player* '(15 16 17 18 19 20 21 22 23 24  1 1 1 1 1 1 1 1 1 1 1 1 1 1 1))
-
-(defun apply-audio-device-selected ()
-  
+(defun set-audio-device ()  
   ;; scan for available devices (just in case)
-  (let ((out-devices (audio-output-devices)))
-    (add-preference :audio :output "Output device" out-devices (car out-devices) nil 'apply-audio-device-selected)
+  (let ((out-devices (juce::audio-driver-output-devices *juce-player* *audio-driver*)))
+    (add-preference :audio :output "Output device" out-devices (car out-devices) nil 'set-audio-device)
     (unless (and (get-pref-value :audio :output)
                  (find (get-pref-value :audio :output) out-devices :test 'string-equal))
       (when (get-pref-value :audio :output) 
         (om-beep-msg "Audio output device: ~S not found. restoring default."  (get-pref-value :audio :output)))
-      (put-default-value (get-pref :audio :output))))
+      (put-default-value (get-pref :audio :output)))
 
-  (juce::setdevices *juce-player* "" 0 ;; default/0 channels for input
-                    (get-pref-value :audio :output) ;; this has changed
-                    (get-pref-value :audio :out-channels) ;; this might be invalidated
-                    (get-pref-value :audio :samplerate) ;; this might be invalidated
-                    (get-pref-value :audio :buffersize)) ;; this might be invalidated
- 
+    (let ((out (or (position (get-pref-value :audio :output) out-devices :test 'string-equal) -1)))
+  
+      (juce::setdevices *juce-player* "" 0 ;; default/0 channels for input
+                        (get-pref-value :audio :output) ;; this has changed
+                        (get-pref-value :audio :out-channels) ;; this might be invalidated
+                        (get-pref-value :audio :samplerate) ;; this might be invalidated
+                        (get-pref-value :audio :buffersize)) ;; this might be invalidated
+      ))
+  
   (let ((device-supported-out-channels (juce::getoutputchannelslist *juce-player*))
         (device-supported-sample-rates (juce::getsamplerates *juce-player*))
         (device-supported-buffer-sizes (juce::getbuffersizes *juce-player*)))
@@ -122,22 +94,22 @@
             "AUDIO SETUP")
   (juce::setdevices  *juce-player* 
                      "" 0
-                     (get-pref-value :audio :output) 
+                     (get-pref-value :audio :output)
                      (get-pref-value :audio :out-channels)
                      (get-pref-value :audio :samplerate)
                      (get-pref-value :audio :buffersize)
                      ))
 
-
 ;;; when this function si called the preferences are set to their default or saved values
 (defun open-juce-player ()
-  (setq *juce-player* (juce::OpenAudioPlayer))
-  (apply-audio-device-selected))
+  (setq *juce-player* (juce::openAudioManager))
+  (juce::setDeviceType *juce-player* *audio-driver*)
+  (set-audio-device))
 
 (om-add-init-fun 'open-juce-player)
 
 (defun close-juce-player ()
-  (juce::closeaudioplayer *juce-player*)
+  (juce::closeAudioManager *juce-player*)
   (setf *juce-player* nil))
 
 (defun configure-audio-channels (list)
