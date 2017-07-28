7b9fa8549ae694190367c2dc8e86a30dcab41b78
Jean Bresson <bresson@ircam.fr>
2017-07-28 08:29:50 +0200

    2D-array slots names

--- om/src/packages/basic/containers/2d-array.lisp
+++ om/src/packages/basic/containers/2d-array.lisp
@@ -2,16 +2,16 @@
 (in-package :om)
 
 (defclass* OMArray () 
-   ((num-elts :initform 1 :initarg :num-elts  :accessor num-elts :documentation "number of elements (a.k.a lines)")
-    (num-fields :initform 1 :initarg :num-fields  :accessor num-fields :documentation "number of fields (a.k.a columns)")
-    (field-names :initform nil :initarg :field-names :accessor field-names :documentation "name of fields")
+   ((num-rows :initform 1 :initarg :num-rows  :accessor num-rows :documentation "number of lines/rows")
+    (num-cols :initform 1 :initarg :num-cols  :accessor num-cols :documentation "number of fields (a.k.a columns)")
+    (col-names :initform nil :initarg :col-names :accessor col-names :documentation "name of fields")
     (data :initform nil :initarg :data :accessor data :documentation "data matrix / list of lists : (col1 col2 ...)")))
 
 (defmethod get-col ((self OMArray) (col integer))
   (nth col (data self)))
 
 (defmethod get-col ((self OMArray) (col string))
-  (let ((pos (position col (field-names self) :test 'string-equal)))
+  (let ((pos (position col (col-names self) :test 'string-equal)))
     (if pos (get-col self pos)
       (om-beep-msg "Field '~A' not found in '~A'" col self))))
 
