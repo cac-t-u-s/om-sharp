de2997749915ebc8093591a014fb83bc6e370ba3
Jean Bresson <bresson@ircam.fr>
2017-10-25 15:51:06 +0200

    typo in license text

--- om/src/packages/tempo/tempo.lisp
+++ om/src/packages/tempo/tempo.lisp
@@ -7,7 +7,7 @@
 ;   This program is free software. For information on usage 
 ;   and redistribution, see the "LICENSE" file in this distribution.
 ;
-;   This program is distributed; in the hope that it will be useful,
+;   This program is distributed in the hope that it will be useful,
 ;   but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
 ;
