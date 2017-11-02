739cd9d4e2e48a94c0042ab8b92e6250997b8b59
Jean Bresson <bresson@ircam.fr>
2017-11-02 15:42:19 +0100

    MIDI colors

--- om/src/packages/midi/midi-setup.lisp
+++ om/src/packages/midi/midi-setup.lisp
@@ -37,22 +37,30 @@
 (add-preference :midi :ports "Configuration" :action 'midi-setup)
 
 (add-preference-section :midi "Channel colors")
-(add-preference :midi :midi-ch1-color " 1" :color (om-random-color))
-(add-preference :midi :midi-ch2-color " 2" :color (om-random-color))
-(add-preference :midi :midi-ch3-color " 3" :color (om-random-color))
-(add-preference :midi :midi-ch4-color " 4" :color (om-random-color))
-(add-preference :midi :midi-ch5-color " 5" :color (om-random-color))
-(add-preference :midi :midi-ch6-color " 6" :color (om-random-color))
-(add-preference :midi :midi-ch7-color " 7" :color (om-random-color))
-(add-preference :midi :midi-ch8-color " 8" :color (om-random-color))
-(add-preference :midi :midi-ch9-color " 9" :color (om-random-color))
-(add-preference :midi :midi-ch10-color "10" :color (om-def-color :gray))
-(add-preference :midi :midi-ch11-color "11" :color (om-random-color))
-(add-preference :midi :midi-ch12-color "12" :color (om-random-color))
-(add-preference :midi :midi-ch13-color "13" :color (om-random-color))
-(add-preference :midi :midi-ch14-color "14" :color (om-random-color))
-(add-preference :midi :midi-ch15-color "15" :color (om-random-color))
-(add-preference :midi :midi-ch16-color "16" :color (om-random-color))
+
+
+(let ((colors (loop for h from 0 to 1 by (/ 1 32) collect (om-make-color-hsv h .8 .78))))
+
+  (add-preference :midi :midi-ch1-color " 1" :color (nth 0 colors))
+  (add-preference :midi :midi-ch2-color " 2" :color (nth 3 colors))
+  (add-preference :midi :midi-ch3-color " 3" :color (nth 5 colors))
+  (add-preference :midi :midi-ch4-color " 4" :color (nth 7 colors))
+  (add-preference :midi :midi-ch5-color " 5" :color (nth 16 colors))
+  (add-preference :midi :midi-ch6-color " 6" :color (nth 18 colors))
+  (add-preference :midi :midi-ch7-color " 7" :color (nth 21 colors))
+  (add-preference :midi :midi-ch8-color " 8" :color (nth 24 colors))
+  (add-preference :midi :midi-ch9-color " 9" :color (nth 29 colors))
+  (add-preference :midi :midi-ch10-color "10" :color (om-def-color :lightsteelblue4))
+  (add-preference :midi :midi-ch11-color "11" :color (om-def-color :darkgreen))
+  (add-preference :midi :midi-ch12-color "12" :color (om-def-color :yellow4))
+  (add-preference :midi :midi-ch13-color "13" :color (om-def-color :burlywood4))
+  (add-preference :midi :midi-ch14-color "14" :color (om-def-color :lightpink4))
+  (add-preference :midi :midi-ch15-color "15" :color (om-def-color :saddlebrown))
+  (add-preference :midi :midi-ch16-color "16" :color (om-def-color :dark-gray))
+  
+  )
+
+;; (color::get-all-color-names)
 
 (defun get-midi-channel-color (n)
   (case n  
