692930d3d1a52b168b8ba967a020cb4368fee760
Jean Bresson <bresson@ircam.fr>
2018-01-13 01:08:34 +0100

    0.0.17

--- om/build/build-om.lisp
+++ om/build/build-om.lisp
@@ -22,7 +22,7 @@
 ;;;=======================================
 
 (defparameter *app-name* "o7")
-(defparameter *om-version* 0.000116)
+(defparameter *om-version* 0.000117)
 (defparameter *beta-release* t)
 (defparameter *version-string* "")
 (defparameter *release-language* :en)
