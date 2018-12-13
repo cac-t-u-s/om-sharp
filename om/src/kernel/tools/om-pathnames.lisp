0ccb2ab2b0e5828829c106faabd45d647c58df49
j-bresson <bresson@ircam.fr>
2018-12-13 21:34:01 +0100

    real-exec-pathname

--- om/src/kernel/tools/om-pathnames.lisp
+++ om/src/kernel/tools/om-pathnames.lisp
@@ -185,14 +185,15 @@
 
 ;;; a utility function to get the executable path from a .app on Mac
 (defun real-exec-pathname (path)
-  (let ((name (car (last (pathname-directory path)))))
-    (if (and (om-directory-pathname-p path)
+  (let* ((path-path (pathname path))
+         (name (car (last (pathname-directory path-path)))))
+    (if (and (om-directory-pathname-p path-path)
              (string-equal "app" (subseq name (- (length name) 3))))
         ;;; path is an application bundle
-        (make-pathname :directory (append (pathname-directory path) (list "Contents" "MacOS"))
+        (make-pathname :directory (append (pathname-directory path-path) (list "Contents" "MacOS"))
                        :name (subseq name 0 (- (length name) 4)))
       ;;; otherwise...
-      path)))
+      path-path)))
 
 
 
