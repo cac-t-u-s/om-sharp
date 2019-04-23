6375bfc98757b21799baf7bc309b051000fb0ef8
Jean Bresson <jean.bresson@ircam.fr>
2019-04-23 19:15:41 +0200

    build utils

--- pack.bat
+++ pack.bat
@@ -3,10 +3,10 @@ mkdir BUILD
 
 xcopy om BUILD\o7\ /E /y
 
-copy "C:\Program Files (x86)\LispWorks\libPortMidi.dll" BUILD\o7\
-copy "C:\Program Files (x86)\LispWorks\libsndfile-1.dll" BUILD\o7\
-copy "C:\Program Files (x86)\LispWorks\libsamplerate.dll" BUILD\o7\
-copy "C:\Program Files (x86)\LispWorks\OMAudioLib.dll" BUILD\o7\
-copy "C:\Program Files (x86)\LispWorks\sdif.dll" BUILD\o7\
+copy "om\resources\lib\win32\libportmidi.dll" BUILD\o7\
+copy "om\resources\lib\win32\libsndfile-1.dll" BUILD\o7\
+copy "om\resources\lib\win32\libsamplerate-0.dll" BUILD\o7\
+copy "om\resources\lib\win32\OMAudioLib.dll" BUILD\o7\
+copy "om\resources\lib\win32\libsdif.dll" BUILD\o7\
 
 
