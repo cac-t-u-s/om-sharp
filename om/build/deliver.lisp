222d12eaa30b267ee20bcd5417dcadc36ac9aff1
j-bresson <bresson@ircam.fr>
2019-08-09 16:34:03 +0200

    Update deliver.lisp

--- om/build/deliver.lisp
+++ om/build/deliver.lisp
@@ -16,7 +16,7 @@
 (print "APPLICATION SETUP")
 (print "==============================")
 
-(defparameter *app-name+version* #-linux "om7-beta" #+linux "om7-alpha")
+(defparameter *app-name+version* "om7-beta" "om7-alpha")
 
 (defparameter *om-directory-folders* (butlast (pathname-directory (current-pathname))))
 
