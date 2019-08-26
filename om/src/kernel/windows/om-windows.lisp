bd11db8282a11186b7c0a633c003c6738c16498d
j-bresson <bresson@ircam.fr>
2019-08-26 17:46:43 +0200

    Update om-windows.lisp

--- om/src/kernel/windows/om-windows.lisp
+++ om/src/kernel/windows/om-windows.lisp
@@ -57,12 +57,12 @@
          (list (om-make-menu-item "Undo" #'(lambda () (funcall (undo-command self))) :key "z" :enabled #'(lambda () (and (undo-command self) t)))
                (om-make-menu-item "Redo" #'(lambda () (funcall (redo-command self))) :key "Z" :enabled #'(lambda () (and (redo-command self) t)))))
         (om-make-menu-comp 
-         (list (om-make-menu-item "Copy" #'(lambda () (funcall (copy-command self))) :key "c" :enabled (and (copy-command self) t))
-               (om-make-menu-item "Cut" #'(lambda () (funcall (cut-command self))) :key "x" :enabled (and (cut-command self) t))
-               (om-make-menu-item "Paste"#'(lambda () (funcall (paste-command self))) :key "v" :enabled (and (paste-command self) t))
+         (list (om-make-menu-item "Copy" #'(lambda () (funcall (copy-command self))) :key "c" :enabled #'(lambda () (and (copy-command self) t)))
+               (om-make-menu-item "Cut" #'(lambda () (funcall (cut-command self))) :key "x" :enabled #'(lambda () (and (cut-command self) t)))
+               (om-make-menu-item "Paste"#'(lambda () (funcall (paste-command self))) :key "v" :enabled #'(lambda () (and (paste-command self) t)))
                (om-make-menu-item "Delete" #'(lambda () (funcall (clear-command self))) :enabled (and (clear-command self) t))))
         (om-make-menu-comp 
-         (list (om-make-menu-item "Select All" #'(lambda () (funcall (select-all-command self))) :key "a" :enabled (and (select-all-command self) t))))
+         (list (om-make-menu-item "Select All" #'(lambda () (funcall (select-all-command self))) :key "a" :enabled #'(lambda () (and (select-all-command self) t)))))
         (om-make-menu-comp 
          (list (om-make-menu 
                 "Font" 
