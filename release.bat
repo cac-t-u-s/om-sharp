bb48326c7fc576e36caa3979a4e3bc3f3d09ad2f
Jean Bresson <jean.bresson@ircam.fr>
2019-09-17 10:01:27 +0200

    Update release.bat

--- release.bat
+++ release.bat
@@ -9,6 +9,10 @@ mkdir BUILD
 
 xcopy om BUILD\om7-beta\ /E /y
 
+del /S BUILD\om7-beta\*.ofasl
+del /S BUILD\om7-beta\*.*~
+del /S BUILD\om7-beta\resources\fonts\*.otf
+
 copy "om\resources\lib\win32\libportmidi.dll" BUILD\om7-beta\
 copy "om\resources\lib\win32\libsndfile-1.dll" BUILD\om7-beta\
 copy "om\resources\lib\win32\libsamplerate-0.dll" BUILD\om7-beta\
