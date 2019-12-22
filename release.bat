f1893655ec6500abea1f77334399fede652bc2e1
Jean Bresson <jean.bresson@ircam.fr>
2019-12-22 23:57:03 +0100

    Update release.bat

--- release.bat
+++ release.bat
@@ -12,9 +12,12 @@ xcopy om BUILD\om-sharp\ /E /y
 del /S BUILD\om-sharp\*.*~
 del /S BUILD\om-sharp\resources\fonts\*.otf
 del /S BUILD\om-sharp\*.ofasl
+del /F/Q/S BUILD\om-sharp\resources\lib\*.* > NUL
+rmdir /Q/S BUILD\om-sharp\resources\lib\
 
 copy "om\resources\lib\win32\libportmidi.dll" BUILD\om-sharp\
 copy "om\resources\lib\win32\libsndfile-1.dll" BUILD\om-sharp\
 copy "om\resources\lib\win32\libsamplerate-0.dll" BUILD\om-sharp\
 copy "om\resources\lib\win32\OMAudioLib.dll" BUILD\om-sharp\
 copy "om\resources\lib\win32\libsdif.dll" BUILD\om-sharp\
+
