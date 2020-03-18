1b5251534f54d0d599963be2a890856735ad0bc2
j-bresson <bresson@ircam.fr>
2020-03-18 22:48:32 +0100

    Declare sections and force order in General preferences

--- om/src/kernel/environment/om-preferences.lisp
+++ om/src/kernel/environment/om-preferences.lisp
@@ -242,6 +242,27 @@
 ;;; special case: sets a behavior from the OM-API
 ;(defmethod set-pref-in-module :after ((module (eql :general)) (key (:eql :listener-on-top)) val) (setf om-lisp::*listener-on-top* val))
 ;;; not anymore (initarg in the constructor)
+(add-preference-section 
+ :general "Patch Execution" 
+ nil
+ '(:catch-errors 
+   :debug 
+   :auto-ev-once-mode
+   ))
+
+(add-preference-section 
+ :general "Lisp REPL" 
+ nil 
+ '(:listener-on-top 
+   :listener-input 
+   :listener-font 
+   :textedit-font 
+   :print-system-output 
+   :om-swank-server 
+   :listener-in-tty
+   ))
+
+
 
 
 
