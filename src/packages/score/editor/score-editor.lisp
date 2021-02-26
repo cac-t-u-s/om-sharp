8ce7ff1803f432905171d4ec40bd5704a8168e80
j-bresson <jean.bresson.mail@gmail.com>
2021-02-26 22:18:36 +0100

    Notify scheduler in key actions of score-editor

--- src/packages/score/editor/score-editor.lisp
+++ src/packages/score/editor/score-editor.lisp
@@ -447,7 +447,9 @@
      (if (om-option-key-p)
          (progn
            (store-current-state-for-undo editor)
-           (score-editor-change-selection-durs editor (if (om-shift-key-p) -1000 -100))
+           (with-schedulable-object
+            (object-value editor)
+            (score-editor-change-selection-durs editor (if (om-shift-key-p) -1000 -100)))
            (editor-invalidate-views editor)
            (report-modifications editor))
        (call-next-method)))
@@ -456,7 +458,9 @@
      (if (om-option-key-p)
          (progn
            (store-current-state-for-undo editor)
-           (score-editor-change-selection-durs editor (if (om-shift-key-p) 1000 100))
+           (with-schedulable-object
+            (object-value editor)
+            (score-editor-change-selection-durs editor (if (om-shift-key-p) 1000 100)))
            (editor-invalidate-views editor)
            (report-modifications editor))
        (call-next-method)))
@@ -478,7 +482,9 @@
      (report-modifications editor))
 
     (:om-key-delete
-     (delete-selection editor))
+     (with-schedulable-object
+      (object-value editor)
+      (delete-selection editor)))
 
     (#\g (add-selection-to-group editor))
     (#\G (delete-selection-group editor))
