2c49813e1f1403ef23291dd3119873e85dc9a353
j-bresson <jean.bresson.mail@gmail.com>
2021-04-03 08:21:15 +0200

    Rewrite midi-track's ADD-NOTE utility

--- src/packages/midi/objects/midi-track.lisp
+++ src/packages/midi/objects/midi-track.lisp
@@ -538,12 +538,13 @@
 ;;;======================================
 
 (defmethod add-note ((object midi-track) note)
-  (with-schedulable-object object
+  (with-schedulable-object
+   object
+   (data-stream-set-frames object
                            (if (midi-events object)
                                (insert-in-order note (midi-events object) :key 'car :test <)
-                             (setf (midi-events object) (list note))))
+                             (list note))))
   (om-invalidate-view object))
-
 
 
 (defun gen-random-midi-notes (n &optional (tmax 10000) (channel 1))
