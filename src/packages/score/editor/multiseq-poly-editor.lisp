9d922fe5d8545953ec441359f5ccad7959ee3144
j-bresson <jean.bresson.mail@gmail.com>
2021-08-12 22:03:37 +0200

    Poly/multi-seq editors: Fix/revise set-interior-size-from-contents
    And add more margin at the bottom.

--- src/packages/score/editor/multiseq-poly-editor.lisp
+++ src/packages/score/editor/multiseq-poly-editor.lisp
@@ -107,19 +107,19 @@
   (let* ((fontsize (editor-get-edit-param self :font-size))
          (unit (font-size-to-unit fontsize))
          (score-view (get-g-component self :main-panel))
-         (shift (car (last (accum-y-shift-list self)))))
+         (bottom (car (last (accum-y-shift-list self))))
+         (margin 5)
+         (y (round (* (+ bottom margin) unit))))
 
-    (when shift
+    (oa::om-set-interior-size score-view
+                              (omp (om-point-x (om-view-size score-view))
+                                   y))
 
-      (oa::om-set-interior-size score-view
-                                (omp (om-point-x (om-view-size score-view))
-                                     (round (* shift unit))))
+    (oa::om-set-interior-size (left-view score-view)
+                              (omp (om-point-x (om-view-size (left-view score-view)))
+                                   y))
+    ))
 
-      (oa::om-set-interior-size (left-view score-view)
-                                (omp (om-point-x (om-view-size score-view))
-                                     (round (* shift unit))))
-
-      )))
 
 (defmethod init-editor-window :after ((self poly-editor-mixin))
   (set-interior-size-from-contents self))
