e8ef9283d33e2fc1e3eabd6eb60e79b710286c10
j-bresson <jean.bresson.mail@gmail.com>
2021-10-02 22:37:54 +0200

    Use -build flag when delivering on Windows
    It doesn't seem to make a difference with -init in practice, but this is
    the recommended way according to LW's manuals.

--- release.bat
+++ release.bat
@@ -1,6 +1,6 @@
 cd build\
 
-"C:\\Program Files (x86)\\LispWorks\\lispworks-7-1-0-x86-win32.exe" -init deliver.lisp
+"C:\\Program Files (x86)\\LispWorks\\lispworks-7-1-0-x86-win32.exe" -build deliver.lisp
 
 cd ..
 
